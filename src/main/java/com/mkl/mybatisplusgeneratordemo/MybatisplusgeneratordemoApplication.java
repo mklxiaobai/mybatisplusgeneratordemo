3ee48fbc7927d0eee63278c0e9fdc7323da65098
mkl <727448352@qq.com>
2020-03-09 16:53:50 +0800

    Initial commit

---
+++ src/main/java/com/mkl/mybatisplusgeneratordemo/MybatisplusgeneratordemoApplication.java
@@ -0,0 +1,13 @@
+package com.mkl.mybatisplusgeneratordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisplusgeneratordemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusgeneratordemoApplication.class, args);
+    }
+
+}
